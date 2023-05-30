b4474d1b6982e1d47d6c65b2c12caa0b669fbc62
Vladimir <Vladimir@Server-iMac.local>
2023-05-30 21:09:07 +0300

    task js

--- src/route/index.js
+++ src/route/index.js
@@ -532,6 +532,7 @@ router.get('/js', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('js', {
+    layout: 'js',
     name: 'JavaScript',
     description:
       'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
